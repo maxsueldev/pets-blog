b3195593162b782e3899a8a7640faa9fa84d1d2d
unknown <maxsuel.dev@gmail.com>
2024-02-14 12:23:06 -0300

    dropdown menu

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ const navListDropDown = document.querySelector('#nav-list-dropdown');
 
 menuHamburger.addEventListener('click', () => {
     navListDropDown.classList.toggle('active');
+    menuHamburger.classList.toggle('active');
     const isActive = navListDropDown.classList.contains('active');
 });
