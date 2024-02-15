b0451a81ea9f05a7031ab41c4ea568610f402de5
unknown <maxsuel.dev@gmail.com>
2024-02-15 17:03:24 -0300

    initial

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Dropdown menu / menu Hamburger
+
 const menuHamburger = document.querySelector('#hamburger');
 const navListDropDown = document.querySelector('#nav-list-dropdown');
 
@@ -5,4 +7,33 @@ menuHamburger.addEventListener('click', () => {
     navListDropDown.classList.toggle('active');
     menuHamburger.classList.toggle('active');
     const isActive = navListDropDown.classList.contains('active');
+});
+
+// Comentários de artigo
+
+const btnEnviarFormComent = document.querySelector('#btn-enviar');
+const nome = document.querySelector('#nome');
+const textareaComentario = document.querySelector('#textarea-comentario');
+const divComentarios = document.querySelector('#comentarios');
+
+btnEnviarFormComent.addEventListener('click tap', (e) => {
+    e.preventDefault();
+
+    if(nome.value != '' && textareaComentario.value != '') {
+        const novaDiv = document.createElement('div');
+        const h3 = document.createElement('h3');
+        const p = document.createElement('p');
+
+        novaDiv.classList = 'coment';
+        h3.innerText = nome.value;
+        p.innerText = textareaComentario.value;
+
+        novaDiv.appendChild(h3);
+        novaDiv.appendChild(p);
+        
+        divComentarios.appendChild(novaDiv);
+
+        nome.value = '';
+        textareaComentario.value = '';
+    }
 });
